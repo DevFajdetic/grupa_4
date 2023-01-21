3365bc96582637e49de52dbaa11e842ed0c3f28d
Mateo Mikulic <mikulic.dev@gmail.com>
2023-01-21 16:25:45 +0100

    working two  ball deliverer, few improvements needed

--- grupa_4/controller.py
+++ grupa_4/controller.py
@@ -23,13 +23,17 @@ class BallDeliverer(Node):
         self.found_ball = False
         self.ball_delivered = False
         self.close_to_obstacle = False
+        self.color_picked = False
         self.image_error_threshold = 0
-        self.current_color = 0
-        self.color = [
-            [2, True], #blue
-            [1, True], #green
-            [1, False] #red
+        self.find_ball_error_threshold = 0
+        self.current_color_index = 2
+        self.colors = [
+            [2, True, 125], #blue
+            #[1, True, 110], #green
+            [1, False, 135], #red
+            [-1, False] #no valid color
         ]
+        self.delivered_colors = [False, False, False]
         self.distance_matrix = [4, 4, 4] #[front, left, right]
         self.declare_parameter('Kp', 0.7)
         self.declare_parameter('f', 265.23)
@@ -40,6 +44,8 @@ class BallDeliverer(Node):
         imGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blurred_img = cv2.medianBlur(imGray, 25)
 
+        self.get_logger().info(f'Current_color: {self.current_color_index}')
+
         if self.found_ball is False:
             self.find_ball(blurred_img)
         elif self.got_ball is False:
@@ -49,6 +55,7 @@ class BallDeliverer(Node):
         elif self.distance_matrix[0] <= 0.25 and self.distance_matrix[0] != 0:
             self.move_robot('x')
         else:
+            self.reset()
             self.move_robot('s')
 
     
@@ -72,18 +79,32 @@ class BallDeliverer(Node):
 
     def find_ball(self, blurred_img): # dodati rotiranje s obzirom na lidar
         self.move_robot('a')
-        self.found_ball = not np.array_equal(self.find_nearest_circle_from_image(blurred_img), np.zeros(3))
+        if not np.array_equal(self.find_nearest_circle_from_image(blurred_img), np.zeros(3)):
+            self.find_ball_error_threshold += 1
+            if self.find_ball_error_threshold == 2:
+                self.found_ball = True
+            return
+        self.find_ball_error_threshold = 0
     
 
     def goto_ball(self, blurred_image, image):
         nearest_circle = self.find_nearest_circle_from_image(blurred_image)        
-        if not np.array_equal(self.find_nearest_circle_from_image(blurred_image), np.zeros(3)):
+        circle_found = not np.array_equal(nearest_circle, np.zeros(3))
+        
+        if circle_found:
             self.image_error_threshold = 0
-            self.set_current_color(image[int(nearest_circle[1]), int(nearest_circle[0])])
-            self.get_logger().info(f'Teske boje: {image[int(nearest_circle[1]), int(nearest_circle[0])]}')
+            circle_center_pixel = image[int(nearest_circle[1]), int(nearest_circle[0])]
+
+            if not self.color_picked:
+                self.pick_color(circle_center_pixel)
+            elif np.argmax(circle_center_pixel) != self.current_color_index:
+                return 
             
-            rotated = self.rotate_robot(self.find_object_angle(nearest_circle[0]))
-            if rotated is True:
+            if not self.found_ball:
+                return
+
+            is_rotated = self.rotate_robot(self.find_object_angle(nearest_circle[0]))
+            if is_rotated:
                 self.move_robot('w')
         else:
             self.image_error_threshold += 1
@@ -93,11 +114,12 @@ class BallDeliverer(Node):
                 self.got_ball = True
 
 
+    def pick_color(self, pixel):
+        self.set_current_color(pixel)
+        self.get_logger().info(f'Teske boje: {pixel}')
+
     def find_goal(self, image):
-        if self.found_goal(image, 10, self.color[self.current_color]) is False:
-            self.get_logger().info('Nisam pronašao!')
-            self.move_robot('d')
-        else:
+        if self.found_goal(image, 10, self.colors[self.current_color_index]):
             self.get_logger().info('Pronađoh!')
             self.move_robot('s')
             if self.close_to_obstacle:
@@ -105,6 +127,9 @@ class BallDeliverer(Node):
                 self.ball_delivered = True
             else:
                 self.move_robot('w')
+        else:
+            self.get_logger().info('Nisam pronašao!')
+            self.move_robot('d')
 
     def move_robot(self, direction):
         movement = Twist()
@@ -152,6 +177,11 @@ class BallDeliverer(Node):
             return rotated
         
     def found_goal(self, img, strip_width, color):
+        if color[0] == -1:
+            self.get_logger().info(f'Nemam boju da trazim goal sefe')
+            return False
+
+        self.get_logger().info(f'{color}')
         img = img[0:img.shape[0], int(img.shape[1]/2 - strip_width):int(img.shape[1]/2 + strip_width)]
         lab_channel = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)[:,:,color[0]]
 
@@ -160,20 +190,46 @@ class BallDeliverer(Node):
         else:
             threshold_type = cv2.THRESH_BINARY
 
-        thresholded_img = cv2.threshold(lab_channel, 127, 255, threshold_type)[1]
+        thresholded_img = cv2.threshold(lab_channel, color[2], 255, threshold_type)[1]
         img = cv2.bitwise_and(img, img, mask = thresholded_img)
 
         self.image_publisher.publish(CvBridge().cv2_to_imgmsg(img, encoding='bgr8'))
-
+        height = 0
+        continuous = False
         zero_array = np.zeros((img.shape[1],3))
         for i in range(img.shape[0]):
             if np.array_equal(img[i], zero_array):
-                return True
+                if continuous is False:
+                    continuous = True
+                
+                if continuous is True:
+                    height +=1
+                    if height == 40:
+                        return True
+            else:
+                continuous = False
 
         return False
     
     def set_current_color(self, pixel):
-        self.current_color = np.argmax(pixel)
+        color_index = np.argmax(pixel)
+        if color_index == 2:
+            color_index = 1
+        black = [75, 75, 75]
+        white = [175, 175, 175]
+        is_black = np.all(np.greater(black, pixel))
+        is_white = np.all(np.less(white, pixel))
+        if is_black or is_white:
+            return False
+        if self.delivered_colors[color_index] is False:
+            self.current_color_index = color_index
+            self.color_picked = True
+            return True
+        else:
+            self.found_ball = False
+        
+        return False
+        
 
     def scan_callback(self, laser_scan):
         angle_matrix = [0, 90, 270]
@@ -182,10 +238,21 @@ class BallDeliverer(Node):
             index = int(np.deg2rad(angle_matrix[i]) / laser_scan.angle_increment)
             self.distance_matrix[i] = ranges[index]
 
-        self.get_logger().info('Ja sam u lidaru')
-        if self.distance_matrix[0] <= 0.12 and self.distance_matrix[0] != 0.:
+        if self.distance_matrix[0] <= 0.18 and self.distance_matrix[0] != 0.:
             self.get_logger().info('Makni zid šefe')
             self.close_to_obstacle = True
+
+    def reset(self):
+        self.delivered_colors[self.current_color_index] = True
+        self.got_ball =  False
+        self.found_ball = False
+        self.ball_delivered = False
+        self.close_to_obstacle = False
+        self.color_picked = False
+        self.image_error_threshold = 0
+        self.find_ball_error_threshold = 0
+        self.current_color_index = 2
+
 
 def main(args=None):
     rclpy.init(args=args)    
